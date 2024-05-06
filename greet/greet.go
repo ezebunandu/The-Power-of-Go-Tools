b79c02470d8a59c3088e2d4a470435cf214e4f16
Samuel Ezebunandu <sam.ezebunandu@paybyphone.com>
2024-05-06 17:02:48 -0600

    implements a greet package

---
+++ greet/greet.go
@@ -0,0 +1,26 @@
+package greet
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+// GreetUser prompts the user for their name and renders a greeting.
+func GreetUser(stdin io.Reader, stdout io.Writer) {
+	name := "you"
+	fmt.Fprintln(stdout, "What is your name?")
+	input := bufio.NewScanner(stdin)
+
+	if input.Scan() {
+		name = input.Text()
+	}
+	fmt.Fprintf(stdout, "Hello, %s.\n", name)
+
+}
+
+func Main() int {
+	GreetUser(os.Stdin, os.Stdout)
+	return 0
+}
