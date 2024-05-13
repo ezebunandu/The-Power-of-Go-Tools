dcc9ddb7a2b3f51a642941af3e8d123c4d5dd8a3
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-05-13 09:15:10 -0600

    Main function pattern

--- hello/cmd/hello/main.go
+++ hello/cmd/hello/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/ezebunandu/hello"
 )
 
 func main() {
-	fmt.Println("Listening on http://localhost:9001")
-	http.ListenAndServe(":9001", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		hello.PrintTo(w)
-	}))
+	hello.Main()
 }
