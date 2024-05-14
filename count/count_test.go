ced9908b7266012ee3026c07c2fe7d31113468fe
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-05-13 23:13:23 -0600

    functional options pattern for counter input and output

--- count/count_test.go
+++ count/count_test.go
@@ -7,13 +7,17 @@ import (
 	"github.com/ezebunandu/count"
 )
 
-func TestCountCountsLines(t *testing.T) {
+func TestLinesCountsLinesInInput(t *testing.T) {
 	t.Parallel()
-	buf := bytes.NewBufferString("1\n2\n3\n")
-	c := count.NewCounter()
-	c.Input = buf
-	got := c.Lines()
+	inputBuf := bytes.NewBufferString("1\n2\n3\n")
+	c, err := count.NewCounter(
+		count.WithInput(inputBuf),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
 	want := 3
+	got := c.Lines()
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
