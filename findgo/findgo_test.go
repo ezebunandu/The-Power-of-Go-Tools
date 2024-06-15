a9c3d4400279d4e791ce6cf7439e8c1238e7041f
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-06-15 15:24:57 -0600

    findgo: working with filesystems

---
+++ findgo/findgo_test.go
@@ -0,0 +1,85 @@
+package findgo_test
+
+import (
+	"archive/zip"
+	"os"
+	"testing"
+	"testing/fstest"
+
+	"github.com/ezebunandu/findgo"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFilesCorrectlyListsFilesInTree(t *testing.T) {
+	t.Parallel()
+	fsys := os.DirFS("testdata/tree")
+	want := []string{
+		"file.go",
+		"subfolder/subfolder.go",
+		"subfolder2/another.go",
+		"subfolder2/file.go",
+	}
+	got := findgo.Files(fsys)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestFilesCorrectlyListFilesInTree(t *testing.T) {
+	t.Parallel()
+	fsys := fstest.MapFS{
+		"file.go":                {},
+		"subfolder/subfolder.go": {},
+		"subfolder2/another.go":  {},
+		"subfolder2/file.go":     {},
+	}
+	want := []string{
+		"file.go",
+		"subfolder/subfolder.go",
+		"subfolder2/another.go",
+		"subfolder2/file.go",
+	}
+	got := findgo.Files(fsys)
+	if !cmp.Equal(want, got) {
+		t.Errorf(cmp.Diff(want, got))
+	}
+}
+
+func BenchmarkFilesOnDisk(b *testing.B) {
+	fsys := os.DirFS("testdata/tree")
+	b.ResetTimer()
+	for range b.N {
+		_ = findgo.Files(fsys)
+	}
+}
+
+func BenchmarkFilesOnMemory(b *testing.B) {
+	fsys := fstest.MapFS{
+		"file.go":                {},
+		"subfolder/subfolder.go": {},
+		"subfolder2/another.go":  {},
+		"subfolder2/file.go":     {},
+	}
+	b.ResetTimer()
+	for range b.N {
+		_ = findgo.Files(fsys)
+	}
+}
+
+func TestFilesCorrectlyListsFilesinZIPArchive(t *testing.T) {
+	t.Parallel()
+	fsys, err := zip.OpenReader("testdata/files.zip")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := []string{
+		"tree/file.go",
+		"tree/subfolder/subfolder.go",
+		"tree/subfolder2/another.go",
+		"tree/subfolder2/file.go",
+	}
+	got := findgo.Files(fsys)
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
