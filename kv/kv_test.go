2b605b7fcb67a86d34ee45dab850cd979dc7e331
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-07-23 22:57:24 -0600

    kv can save persistently to disk

--- kv/kv_test.go
+++ kv/kv_test.go
@@ -50,3 +50,32 @@ func TestSetUpdatesExistingKeyToNewValue(t *testing.T) {
 		t.Errorf("expected 'new value' got %v", v)
 	}
 }
+
+func TestSaveSavesDataPersistently(t *testing.T) {
+	t.Parallel()
+	path := t.TempDir() + "/kvtest.store"
+	s, err := kv.OpenStore(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	s.Set("A", "1")
+	s.Set("B", "2")
+	s.Set("C", "3")
+	err = s.Save()
+	if err != nil {
+		t.Fatal(err)
+	}
+	s2, err := kv.OpenStore(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v, _ := s2.Get("A"); v != "1" {
+		t.Errorf("want A=1, got %s", v)
+	}
+	if v, _ := s2.Get("B"); v != "2" {
+		t.Errorf("want B=2, got %s", v)
+	}
+	if v, _ := s2.Get("C"); v != "3" {
+		t.Errorf("want C=3, got %s", v)
+	}
+}
